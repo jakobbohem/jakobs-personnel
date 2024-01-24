7b9dcf86ef3733735ef616ebd64c94740f5df4a5
Jakob Ryden <jakob.ryden@mojang.com>
2024-01-24 13:46:09 +0100

    Added team and craft fields to Person for later reverse lookup and stats

--- person.py
+++ person.py
@@ -1,11 +1,13 @@
 class Person:
-    def __init__(self, name, role, email, github, work_area, employer):
+    def __init__(self, name, role, email, github, work_area, employer, team, craft):
         self.name = name
         self.role = role
         self.email = email
         self.github = github
         self.work_area = work_area
         self.employer = employer
+        self.team = team
+        self.craft = craft
 
     def match_field(self, field, value):
         return value.lower() in str(getattr(self, field)).lower()
